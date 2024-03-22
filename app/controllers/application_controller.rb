e8f8ad372219148782f397a71012c849f400128c
Chaitanya Gupta <chaitanya.gupta@veersatech.com>
2024-03-22 18:33:28 +0530

    metadata for the url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   def set_link
-    id = Base62.decode(params[:id])
-    @link = Link.find(id)
+    @link = Link.find(params[:id])
   end
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,20 +1,40 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: [:show]
+  before_action :set_link, only: [:show, :edit, :update, :destroy]
   def index
     @links = Link.recent_first
+    @link = Link.new
   end
 
   def create
     @link = Link.new(link_params)
     if @link.save
-      redirect_to root_path
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.turbo_stream { render turbo_stream: turbo_stream.prepend("links", @link) }
+      end
     else
       render :index, status: :unprocessable_entity
     end
   end
 
   def show
+  end
 
+  def edit
+  end
+
+  def update
+    if @link.update(link_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @link.destroy
+    redirect_to root_path
+    flash[:notice] = "Link was successfully destroyed."
   end
 
   private
