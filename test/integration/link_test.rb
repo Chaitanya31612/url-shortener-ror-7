0d1138772b5ac076c9fed4399f448a376fd66ac6
Chaitanya Gupta <chaitanya.gupta@veersatech.com>
2024-03-29 18:06:11 +0530

    integration test for link

---
+++ test/integration/link_test.rb
@@ -0,0 +1,63 @@
+require "test_helper"
+
+class LinkTest < ActionDispatch::IntegrationTest
+  test "cannot create link without url" do
+    post links_path, params: {link: {url: ""}}
+    assert_response :unprocessable_entity
+  end
+
+  test "can create link as a guest" do
+    post links_path, params: {link: {url: "https://example.com"}}
+    assert_response :redirect
+  end
+
+  test "can create link as a guest (with turbo response)" do
+    assert_difference "Link.count" do
+      post links_path(format: :turbo_stream), params: {link: {url: "https://example.com"}}
+      assert_response :ok
+      assert_nil Link.last.user_id
+    end
+  end
+
+  test "can create link as a owner/user" do
+    user = users(:one)
+    sign_in user
+    assert_difference "Link.count" do
+      post links_path(format: :turbo_stream), params: {link: {url: "https://example.com"}}
+      assert_response :ok
+      assert_equal user.id, Link.last.user_id
+    end
+  end
+
+  test "can edit link as a owner/user" do
+    user = users(:one)
+    sign_in user
+
+    link = links(:one)
+    post edit_link_path(format: :turbo_stream), params: {link: {url: "https://example.com"}}
+    assert_response :ok
+    assert_equal user.id, Link.last.user_id
+  end
+
+  test "can create link as a owner/user" do
+    user = users(:one)
+    sign_in user
+    assert_difference "Link.count" do
+      post links_path(format: :turbo_stream), params: {link: {url: "https://example.com"}}
+      assert_response :ok
+      assert_equal user.id, Link.last.user_id
+    end
+  end
+
+  test "can create link as a owner/user" do
+    user = users(:one)
+    sign_in user
+    assert_difference "Link.count" do
+      post links_path(format: :turbo_stream), params: {link: {url: "https://example.com"}}
+      assert_response :ok
+      assert_equal user.id, Link.last.user_id
+    end
+  end
+
+
+end
